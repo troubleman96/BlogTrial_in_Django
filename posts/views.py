de1af2de708bed1a0cf2b80d2ecb736c67c40b26
troubleman96 <itslugenge96@gmail.com>
2025-08-15 15:53:55 +0300

    updateeeesss

--- posts/views.py
+++ posts/views.py
@@ -5,14 +5,7 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from .serializers import PostSerializer
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
 
-    def list(self, request:Request):
-        queryset = Post.objects.all()
-        serializer = PostSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def retrieve(self, request:Request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
