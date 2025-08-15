4582d5be5af538da20481a3972b116b73b391629
troubleman96 <itslugenge96@gmail.com>
2025-08-15 23:38:04 +0300

    jwt intro

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.request import Request
 from .serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
